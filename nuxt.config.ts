01e3a58dd3b460bb0d27c677051df72a47eba41b
saintus-create <bradleydip@gmail.com>
2026-06-01 20:09:50 -0700

    Add docus configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,7 @@
 export default defineNuxtConfig({
   extends: ['docus'],
+  docus: {
+    title: 'Encyclopedia of Rhetoric and Composition',
+    description: 'Communication from Ancient Times to the Information Age',
+  },
 })
